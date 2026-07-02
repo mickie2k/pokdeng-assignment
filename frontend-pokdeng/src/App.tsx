c337a9f2bd1bb9319123b56dbcfc3d0f529c6b87
mickie2k <tpookongmek@gmail.com>
2026-07-02 15:26:48 +0700

    feat: start lobby

--- frontend-pokdeng/src/App.tsx
+++ frontend-pokdeng/src/App.tsx
@@ -3,32 +3,58 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import axios from 'axios';
+import Game from './components/Game'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const [balance, setBalance] = useState(1000)
+  const [game, setGame] = useState(null)
+  const handleStartGame = async () => {
+    try {
+      const response = await axios.post(apiUrl + '/game/start', {
+        initialBalance: balance,
+      });
+      console.log('Game started:', response.data);
+      setGame(response.data);
+    } catch (error) {
+      console.error('Error starting game:', error);
+      if (axios.isAxiosError(error)) {
+        alert('Error starting game: ' + (error.response?.data?.error || error.message));
+      }
+    }
+  }
+
 
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+        {game ? <Game /> : <>
+
+          <div className="hero">
+            <img src={heroImg} className="base" width="170" height="179" alt="" />
+            <img src={reactLogo} className="framework" alt="React logo" />
+            <img src={viteLogo} className="vite" alt="Vite logo" />
+          </div>
+          <div>
+            <h1>Welcome to Pokdeng</h1>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label>Initial Balance:</label>
+            <input required className="counter bg-black" type="number" value={balance} onChange={(e) => setBalance(Number(e.target.value))} />
+
+            <button
+              type="button"
+              className="bg-purple-600 hover:bg-purple-700 cursor-pointer text-white font-bold py-2 px-4 rounded"
+              onClick={() => handleStartGame()}
+            >
+              Start Game
+            </button>
+          </div>
+        </>
+        }
+      </section >
 
     </>
   )
---
+++ frontend-pokdeng/src/components/Game.tsx
@@ -0,0 +1,11 @@
+function Game() {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default Game;
